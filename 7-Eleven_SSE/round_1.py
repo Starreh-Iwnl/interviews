de5e7d022df2ab83d634250ddaf460794e07adc9
Sounak Roy <iam.sounakroy@gmail.com>
2024-07-04 16:25:34 +0530

    7-Eleven SSE (Round 1)

---
+++ 7-Eleven_SSE/round_1.py
@@ -0,0 +1,39 @@
+"""
+Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals, and return an array of the non-overlapping intervals that cover all the intervals in the input.
+ 
+Example 1:
+Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
+Output: [[1,6],[8,10],[15,18]]
+Explanation: Since intervals [1,3] and [2,6] overlap, merge them into [1,6].
+ 
+Example 2:
+Input: intervals = [[1,4],[4,5]]
+Output: [[1,5]]
+Explanation: Intervals [1,4] and [4,5] are considered overlapping.
+"""
+
+
+def solve(intervals):
+    intervals.sort(key = lambda i: i[0])
+    res = [intervals[0]]
+
+    for start, end in intervals:
+        lastEnd = res[-1][1]
+
+        if start <= lastEnd:
+            res[-1][1] = max(end, lastEnd)
+        else:
+            res.append([start, end])
+    
+    return res
+
+
+def main():
+    intervals = [[1,3],[2,6],[8,10],[15,18]]
+    res = solve(intervals)
+
+    print(res)
+
+
+if __name__ == "__main__":
+    main()
